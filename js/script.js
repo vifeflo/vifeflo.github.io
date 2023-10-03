59b7de9eea5675ef5aad57430f2aa7753eab7f50
Vinicius Ferreira Florencio <viniciusferreiraflorencio@live.com>
2023-10-03 15:39:57 -0300

    Embed Spotify

--- js/script.js
+++ js/script.js
@@ -10,3 +10,7 @@ document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click",
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    //
+  };
